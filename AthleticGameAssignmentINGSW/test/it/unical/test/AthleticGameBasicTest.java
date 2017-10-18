0a0eafe657eb5b1f923729ef0cdcc1f67b792fb1
ADAMONE <ADAMONE@ValarMorghulis.mshome.net>
2017-10-18 13:23:28 +0200

    es. in lab

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,36 @@
 package it.unical.test;
 
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
 public class AthleticGameBasicTest {
 
+	private static AthleticGame game;
+	
+	@BeforeClass
+	public static void init(){
+		game = new AthleticGame("test");
+	}
+	
+	@Test
+	public void addArrivalTest() {
+		
+		game.addArrival("mario", Instant.now().plusMillis(1000));
+		
+		Assert.assertEquals(game.getArrivals().size(), 1);
+		
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void getParecipiantTimeTest(){
+		
+		game.getParecipiantTime("marioo");
+
+	}
+	
 }
